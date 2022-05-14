7e738a1cc69aaea8c3bdab5042f4c8227923a5b0
Praveenkumar Bouna <62539682+CodeWithPraveen@users.noreply.github.com>
2022-05-14 08:06:48 +0800

    Adding Section07-LinqToJson

---
+++ Section07-LinqToJson/CMS.Data.Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CMS.Data.Models
+{
+    public class Student
+    {
+        public Student(int studentId, string firstName, string lastName, int courseId = 0)
+        {
+            this.StudentId = studentId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.CourseId = courseId;
+        }
+
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CourseId { get; set; }
+    }
+
+    public class EngineeringStudent : Student
+    {
+        public EngineeringStudent(int studentId, string firstName, string lastName, int courseId = 0) 
+        : base(studentId, firstName, lastName, courseId)
+        {
+        }
+    }
+
+    public class MedicalStudent : Student
+    {
+        public MedicalStudent(int studentId, string firstName, string lastName, int courseId = 0) 
+        : base(studentId, firstName, lastName, courseId)
+        {
+        }
+    }
+}
